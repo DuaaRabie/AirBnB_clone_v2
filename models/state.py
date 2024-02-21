67640c4be11d9b475d40fd010dad0682f8e2c700
Duaa Rabie <Duaarabie11@gmail.com>
2024-02-21 14:48:24 -0800

    update code

--- models/state.py
+++ models/state.py
@@ -5,6 +5,21 @@ from models.base_model import BaseModel
 
 class State(BaseModel):
     """ State class """
+    user_id = ""
+    name = ""
+    description = ""
+    number_rooms = 0
+    number_bathrooms = 0
+    max_guest = 0
+    price_by_night = 0
+    latitude = 0.0
+    longitude = 0.0
+    amenity_ids = []
+
     def __init__(self, *args, **kwargs):
-        self.__dict__.update(kwargs)
+        params = {}
+        for key, val in enumerate(kwargs):
+            if key in self.__dict__:
+                params[key] = val
+        self.__dict__.update(params)
         super().__init__()
