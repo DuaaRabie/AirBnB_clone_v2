91a87d1954e5049759faa635b60c30d82930109a
Duaa Rabie <Duaarabie11@gmail.com>
2024-02-22 19:05:10 -0800

    task4

---
+++ setup_mysql_test.sql
@@ -0,0 +1,16 @@
+-- This script prepare MySQL server for the project
+
+-- Create the database if it doesn't exist
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+
+-- Create the user if it doesn't exist
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+
+-- Grant all privileges on the specified database to the user
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+
+-- Grant SELECT privilege on the performance_schema database to the user
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
+
+-- Flush privileges to ensure that the changes take effect immediately
+FLUSH PRIVILEGES;
