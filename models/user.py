281312ed408e2991c72d491b476f76f9c1210905
Duaa Rabie <DuaaRabie11@gmail.com>
2024-12-07 11:22:58 -0800

    updaet code

--- models/user.py
+++ models/user.py
@@ -8,7 +8,7 @@ class User(BaseModel, Base):
     """This class defines a user by various attributes"""
     __tablename__ = "users"
     email = Column(String(128), nullable=False)
-    password = Column(String(128), nullable=False)
+    _password = Column("password", String(128), nullable=False)
     first_name = Column(String(128), nullable=True)
     last_name = Column(String(128), nullable=True)
     
@@ -16,3 +16,8 @@ class User(BaseModel, Base):
         if kwargs:
             self.__dict__.update(kwargs)
         super().__init__()
+
+    @property
+    def password(self):
+        """ getter for password """
+        return self._password.replace("_", " ")
